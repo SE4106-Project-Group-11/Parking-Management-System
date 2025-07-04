d76a89d9c5a660a16157b51bafd7197dcc38829e
viyash-24 <viyashviyashan2417@gmail.com>
2025-07-04 22:20:34 +0530

    Comment added

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function(requiredRole = null) {
   return (req, res, next) => {
     const auth = req.headers.authorization;
     if (!auth) return res.status(401).json({ message: 'Missing Authorization header' });
-
+    // Check if the token is in the format
     const token = auth.split(' ')[1];
     try {
       const payload = jwt.verify(token, JWT_SECRET);
